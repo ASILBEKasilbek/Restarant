88fc593700e1a688450b94e601234be957153f76
Asilbek <148743219+ASILBEKasilbek@users.noreply.github.com>
2025-07-20 16:06:32 +0500

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register()
+admin.site.register(Restaraund)
+admin.site.register(Category)
+admin.site.register(Order)
 # Register your models here.
